07d61676a5bccc4b741a4e477de39cba05bbb2eb
dolby360 <dolevben054@gmail.com>
2022-09-04 12:43:49 +0300

    start high level desigen

---
+++ ast.h
@@ -0,0 +1,4 @@
+
+enum node_type_e {
+    LABLE,
+};
---
+++ get_link.c
@@ -0,0 +1,25 @@
+/*
+This component getting node and transform it into a like.
+For example:
+assignment
+    -> lable
+    -> Help Wanted
+assignment
+    -> user
+    -> dolby360
+assignment
+    -> state
+    -> open
+assignment
+    -> type
+    -> issues
+
+For the above ast, this program should give:
+https://github.com/search?q=label%3A%22Help+Wanted%22+%40dolby360&state=open&type=Issues
+
+URL to ascii:
+%3A => :
+%22 => "
+%40 => @
+*/
+
